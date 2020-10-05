6a94ebe23697dec35a414e0ee0e801f08e8a7e3a
Sakal Patil <Sakal Patil@DESKTOP-UU422TR>
2020-10-05 19:51:43 +0530

    Add project files.

---
+++ StudentApiWithSPs/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StudentApiWithSPs.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ StudentApiWithSPs/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentApiWithSPs.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
